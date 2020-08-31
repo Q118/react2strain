214ecf991ec327618bb33f8482d853eb392ecd36
Q118 <shelbyfish91@gmail.com>
2020-08-31 16:38:14 -0700

    event handlers set up

--- src/Strain.js
+++ src/Strain.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-export default function strain() {
+export default function Strain() {
     return (
         <div>
             <label>
--- src/StrainList.js
+++ src/StrainList.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import Strain from './Strain';
 
-export default function strainList() {
-    return (
-        <Strain />
-    )
+export default function strainList({ strains }) {
+    return strains.map((strain) => {
+        return <Strain key={strain.id} strain={strain} />;
+    });
 }
