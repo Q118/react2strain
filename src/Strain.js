839c5dd806d3fe432978a932f766b036927f073d
Q118 <shelbyfish91@gmail.com>
2020-08-31 16:07:44 -0700

    creating initial components

---
+++ src/Strain.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function strain() {
+    return (
+        <div>
+            <label>
+                <input
+                type="checkbox"
+                />
+                Strain Name
+            </label>
+        </div>
+    )
+}
---
+++ src/StrainList.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import Strain from './Strain';
+
+export default function strainList() {
+    return (
+        <Strain />
+    )
+}
