885d0b08d1d807de5af30b088f034791e62300b4
Q118 <shelbyfish91@gmail.com>
2020-08-31 16:15:50 -0700

    keys set up for strain list and placeholders

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
-import React from 'react';
-import StrainList from './StrainList';
+/** @format */
+
+import React, {useState, useRef, useEffect} from "react";
+import StrainList from "./StrainList";
+import { v4 as uuidv4 } from 'uuid';
 
 function App() {
-  return (
-   <>
-<StrainList />
-<input type="text" placeholder= "e.g. OG Kush"/>
-</>
-  );
+	return (
+		<>
+			<StrainList />
+			<input type="text" placeholder="e.g. OG Kush" />
+			<button>Add to Stash</button>
+			<button>Clear Smoked Strains </button>
+			<div>Smoked away 0 strains.</div>
+		</>
+	);
 }
 
 export default App;
